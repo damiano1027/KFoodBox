4567d7dad2b57eca413e65d2bdca98afe1550c04
김주원 <damiano102777@naver.com>
2024-02-17 14:16:11 +0900

    init: 프로젝트 생성

---
+++ src/main/java/kfoodbox/KFoodBoxApplication.java
@@ -0,0 +1,13 @@
+package kfoodbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KFoodBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KFoodBoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kfoodbox/KFoodBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package kfoodbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KFoodBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
