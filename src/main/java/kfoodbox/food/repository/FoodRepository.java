dd093638e0a704fa2ff70692ef3b6285a3cdc4a2
김주원 <damiano102777@naver.com>
2024-03-16 19:57:44 +0900

    feat: 모든 음식카테고리 조회 Repository

--- src/main/java/kfoodbox/food/repository/FoodRepository.java
+++ src/main/java/kfoodbox/food/repository/FoodRepository.java
@@ -1,5 +1,6 @@
 package kfoodbox.food.repository;
 
+import kfoodbox.food.dto.response.AllFoodCategoriesResponse;
 import kfoodbox.food.dto.response.FoodsResponse;
 import org.apache.ibatis.annotations.Param;
 
@@ -7,4 +8,5 @@ import java.util.List;
 
 public interface FoodRepository {
     List<FoodsResponse.Food> findFoodsByCategoryId(@Param("id") Long id);
+    List<AllFoodCategoriesResponse.FoodCategory> findAllFoodCategories();
 }
