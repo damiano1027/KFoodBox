a98caf4925adde4a65d665534cbdc88e383e53e1
김주원 <damiano102777@naver.com>
2024-03-16 19:32:31 +0900

    feat: 특정 카테고리의 모든 음식 조회 Controller

--- src/main/java/kfoodbox/food/controller/FoodController.java
+++ src/main/java/kfoodbox/food/controller/FoodController.java
@@ -1,11 +1,20 @@
 package kfoodbox.food.controller;
 
+import kfoodbox.food.dto.response.FoodsResponse;
 import kfoodbox.food.service.FoodService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class FoodController {
     private final FoodService foodService;
+
+    @GetMapping("/food-categories/{id}/foods")
+    public ResponseEntity<FoodsResponse> findFoodsInCategory(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(foodService.findFoodsInCategory(id));
+    }
 }
