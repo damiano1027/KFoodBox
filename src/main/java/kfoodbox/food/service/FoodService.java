6d670934a13d291b99f9a4522c937d1de7cf7416
김주원 <damiano102777@naver.com>
2024-03-16 19:57:56 +0900

    feat: 모든 음식카테고리 조회 Service

--- src/main/java/kfoodbox/food/service/FoodService.java
+++ src/main/java/kfoodbox/food/service/FoodService.java
@@ -1,7 +1,9 @@
 package kfoodbox.food.service;
 
+import kfoodbox.food.dto.response.AllFoodCategoriesResponse;
 import kfoodbox.food.dto.response.FoodsResponse;
 
 public interface FoodService {
     FoodsResponse findFoodsInCategory(Long id);
+    AllFoodCategoriesResponse findAllFoodCategories();
 }
