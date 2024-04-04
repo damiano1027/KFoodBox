9be67cbe6fe14a0760844962d47847ee73411bb0
김주원 <damiano102777@naver.com>
2024-04-04 19:41:37 +0900

    feat: UserUpdateRequest dto에 데이터 제약조건 추가

--- src/main/java/kfoodbox/user/dto/request/UserUpdateRequest.java
+++ src/main/java/kfoodbox/user/dto/request/UserUpdateRequest.java
@@ -1,9 +1,13 @@
 package kfoodbox.user.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UserUpdateRequest {
+    @NotBlank @Size(min = 1, max = 8)
     private String nickname;
+    @NotBlank
     private String password;
 }
