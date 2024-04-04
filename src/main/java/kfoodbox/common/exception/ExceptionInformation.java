f98592072f65fbfaa3e3b1c1a471e45081e4e21d
김주원 <damiano102777@naver.com>
2024-04-04 13:59:28 +0900

    feat: 로그인시의 에러 메시지 추가

--- src/main/java/kfoodbox/common/exception/ExceptionInformation.java
+++ src/main/java/kfoodbox/common/exception/ExceptionInformation.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ExceptionInformation {
     EMAIL_DUPLICATES("중복되는 이메일이 이미 존재합니다.", HttpStatus.CONFLICT),
     NICKNAME_DUPLICATES("중복되는 닉네임이 이미 존재합니다.", HttpStatus.CONFLICT),
-    NON_EXISTENT_LANGUAGE("존재하지 않는 언어입니다.", HttpStatus.NOT_FOUND);
+    NON_EXISTENT_LANGUAGE("존재하지 않는 언어입니다.", HttpStatus.NOT_FOUND),
+    NO_MEMBER("회원 정보가 존재하지 않습니다.", HttpStatus.NOT_FOUND);
 
     private final String message;
     private final HttpStatus httpStatus;
