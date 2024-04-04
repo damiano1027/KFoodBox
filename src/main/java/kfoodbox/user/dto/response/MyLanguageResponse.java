29eb36f6f381db6041a26ce821f0eff59c5f290e
김주원 <damiano102777@naver.com>
2024-04-04 19:51:38 +0900

    feat: MyLanguageResponse dto 정의

---
+++ src/main/java/kfoodbox/user/dto/response/MyLanguageResponse.java
@@ -0,0 +1,10 @@
+package kfoodbox.user.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MyLanguageResponse {
+    private Long id;
+}
