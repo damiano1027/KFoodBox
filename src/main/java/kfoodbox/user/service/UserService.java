f2ecf392ab102b7800aa227b346641dc58a2b8a1
김주원 <damiano102777@naver.com>
2024-04-04 11:45:31 +0900

    feat: 모든 언어 조회 서비스 로직

--- src/main/java/kfoodbox/user/service/UserService.java
+++ src/main/java/kfoodbox/user/service/UserService.java
@@ -2,8 +2,10 @@ package kfoodbox.user.service;
 
 import kfoodbox.user.dto.request.SignupRequest;
 import kfoodbox.user.dto.response.EmailExistenceResponse;
+import kfoodbox.user.dto.response.LanguagesResponse;
 
 public interface UserService {
     EmailExistenceResponse getExistenceOfNickname(String email);
     void signUp(SignupRequest request);
+    LanguagesResponse getAllLanguages();
 }
