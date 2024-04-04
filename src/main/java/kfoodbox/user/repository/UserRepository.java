c8795bfdf29d616d92f8bae4c27760a74fc28262
김주원 <damiano102777@naver.com>
2024-04-04 11:45:06 +0900

    feat: 모든 언어 조회 쿼리

--- src/main/java/kfoodbox/user/repository/UserRepository.java
+++ src/main/java/kfoodbox/user/repository/UserRepository.java
@@ -4,9 +4,12 @@ import kfoodbox.user.entity.Language;
 import kfoodbox.user.entity.User;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 public interface UserRepository {
     void saveUser(@Param("user") User user);
     User findUserByEmail(@Param("email") String email);
     User findUserByNickname(@Param("nickname") String nickname);
     Language findLanguageById(@Param("id") Long id);
+    List<Language> findAllLanguages();
 }
