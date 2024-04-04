116ee18e6d87a9e58970580ba3fdb5672e94402d
김주원 <damiano102777@naver.com>
2024-04-04 11:21:32 +0900

    feat: SingupRequest dto 클래스 데이터 유효성 검증 추가

--- src/main/java/kfoodbox/user/dto/request/SignupRequest.java
+++ src/main/java/kfoodbox/user/dto/request/SignupRequest.java
@@ -1,11 +1,19 @@
 package kfoodbox.user.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class SignupRequest {
+    @NotBlank @Email
     private String email;
+    @NotBlank
     private String password;
+    @NotBlank @Size(min = 1, max = 8)
     private String nickname;
+    @NotNull
     private Long languageId;
 }
