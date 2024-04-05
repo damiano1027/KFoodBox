f94ba4d77a5a7779cd0069744db73eeb2036e4ab
김주원 <damiano102777@naver.com>
2024-04-05 21:34:58 +0900

    feat: 자유게시판 글 북마크 생성 서비스 로직

--- src/main/java/kfoodbox/bookmark/service/BookmarkService.java
+++ src/main/java/kfoodbox/bookmark/service/BookmarkService.java
@@ -1,10 +1,12 @@
 package kfoodbox.bookmark.service;
 
+import kfoodbox.bookmark.dto.request.CommunityArticleBookmarkCreateRequest;
 import kfoodbox.bookmark.dto.response.MyCommunityArticleBookmarksResponse;
 import kfoodbox.bookmark.dto.response.MyCustomRecipeArticleBookmarksResponse;
 import kfoodbox.bookmark.dto.response.MyFoodBookmarksResponse;
 
 public interface BookmarkService {
+    void createCommunityArticleBookmark(CommunityArticleBookmarkCreateRequest request);
     MyCommunityArticleBookmarksResponse getMyCommunityArticleBookmarks();
     MyCustomRecipeArticleBookmarksResponse getMyCustomRecipeArticleBookmarks();
     MyFoodBookmarksResponse getMyFoodBookmarks();
--- src/main/java/kfoodbox/bookmark/service/BookmarkServiceImpl.java
+++ src/main/java/kfoodbox/bookmark/service/BookmarkServiceImpl.java
@@ -1,12 +1,17 @@
 package kfoodbox.bookmark.service;
 
 import jakarta.servlet.http.HttpServletRequest;
+import kfoodbox.article.entity.CommunityArticle;
+import kfoodbox.article.repository.CommunityArticleRepository;
+import kfoodbox.bookmark.dto.request.CommunityArticleBookmarkCreateRequest;
 import kfoodbox.bookmark.dto.response.MyCommunityArticleBookmarksResponse;
 import kfoodbox.bookmark.dto.response.MyCustomRecipeArticleBookmarksResponse;
 import kfoodbox.bookmark.dto.response.MyFoodBookmarksResponse;
+import kfoodbox.bookmark.entity.CommunityArticleBookmark;
 import kfoodbox.bookmark.repository.BookmarkRepository;
 import kfoodbox.common.exception.CriticalException;
 import kfoodbox.common.exception.ExceptionInformation;
+import kfoodbox.common.exception.NonCriticalException;
 import kfoodbox.common.request.RequestApproacher;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,6 +21,32 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class BookmarkServiceImpl implements BookmarkService {
     private final BookmarkRepository bookmarkRepository;
+    private final CommunityArticleRepository communityArticleRepository;
+
+    @Override
+    @Transactional
+    public void createCommunityArticleBookmark(CommunityArticleBookmarkCreateRequest request) {
+        HttpServletRequest servletRequest = RequestApproacher.getHttpServletRequest();
+        Long userId = (Long) servletRequest.getAttribute("userId");
+
+        if (userId == null) {
+            throw new CriticalException(ExceptionInformation.INTERNAL_SERVER_ERROR);
+        }
+
+        CommunityArticle communityArticle = communityArticleRepository.findCommunityArticleById(request.getCommunityArticleId());
+        if (communityArticle == null) {
+            throw new NonCriticalException(ExceptionInformation.NO_ARTICLE);
+        }
+
+        CommunityArticleBookmark existingBookmark = bookmarkRepository.findCommunityArticleBookmarkByUserIdAndCommunityArticleId(userId, request.getCommunityArticleId());
+        if (existingBookmark != null) {
+            throw new NonCriticalException(ExceptionInformation.BOOKMARK_DUPLICATES);
+        }
+
+        CommunityArticleBookmark newBookmark = CommunityArticleBookmark.from(request);
+        newBookmark.changeUserId(userId);
+        bookmarkRepository.saveCommunityArticleBookmark(newBookmark);
+    }
 
     @Override
     @Transactional(readOnly = true)
