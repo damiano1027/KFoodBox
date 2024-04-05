2d49696aaa3d34eca31412c50b0c924b2ea4fa61
김주원 <damiano102777@naver.com>
2024-04-05 22:14:44 +0900

    feat: 음식 엔티티 조회 쿼리

--- src/main/java/kfoodbox/food/repository/FoodRepository.java
+++ src/main/java/kfoodbox/food/repository/FoodRepository.java
@@ -4,12 +4,14 @@ import kfoodbox.food.dto.response.AllFoodCategoriesResponse;
 import kfoodbox.food.dto.response.FoodCategoryResponse;
 import kfoodbox.food.dto.response.FoodResponse;
 import kfoodbox.food.dto.response.FoodsResponse;
+import kfoodbox.food.entity.Food;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface FoodRepository {
     FoodResponse findFoodById(@Param("id") Long id);
+    Food findFoodEntityById(@Param("id") Long id);
     List<FoodsResponse.Food> findFoodsByCategoryId(@Param("id") Long id);
     FoodCategoryResponse findFoodCategoryById(@Param("id") Long id);
     List<AllFoodCategoriesResponse.FoodCategory> findAllFoodCategories();
