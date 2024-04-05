5b065f18c78988e3586bb8a410f8d3a23370739a
김주원 <damiano102777@naver.com>
2024-04-05 20:37:11 +0900

    feat: 특정 회원의 레시피 게시판 북마크 리스트 조회 쿼리

--- src/main/java/kfoodbox/bookmark/repository/BookmarkRepository.java
+++ src/main/java/kfoodbox/bookmark/repository/BookmarkRepository.java
@@ -1,10 +1,12 @@
 package kfoodbox.bookmark.repository;
 
 import kfoodbox.bookmark.dto.response.MyCommunityArticleBookmarksResponse;
+import kfoodbox.bookmark.dto.response.MyCustomRecipeArticleBookmarksResponse;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface BookmarkRepository {
     List<MyCommunityArticleBookmarksResponse.Bookmark> findBookmarkCommunityArticlesByUserId(@Param("id") Long id);
+    List<MyCustomRecipeArticleBookmarksResponse.Bookmark> findBookmarkCustomRecipeArticlesByUserId(@Param("id") Long id);
 }
