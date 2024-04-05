4af0849da6c08afd80c2545529825c73a13326b5
김주원 <damiano102777@naver.com>
2024-04-05 20:17:58 +0900

    feat: 특정 회원의 자유게시판 북마크 리스트 조회 API Controller 메소드

--- src/main/java/kfoodbox/bookmark/controller/BookmarkController.java
+++ src/main/java/kfoodbox/bookmark/controller/BookmarkController.java
@@ -1,11 +1,22 @@
 package kfoodbox.bookmark.controller;
 
+import kfoodbox.bookmark.dto.response.MyCommunityArticleBookmarksResponse;
 import kfoodbox.bookmark.service.BookmarkService;
+import kfoodbox.common.authority.Authority;
+import kfoodbox.common.authority.Login;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class BookmarkController {
     private final BookmarkService bookmarkService;
+
+    @Login(Authority.NORMAL)
+    @GetMapping("/my-community-article-bookmarks")
+    public ResponseEntity<MyCommunityArticleBookmarksResponse> getMyCommunityArticleBookmarks() {
+        return ResponseEntity.ok(bookmarkService.getMyCommunityArticleBookmarks());
+    }
 }
