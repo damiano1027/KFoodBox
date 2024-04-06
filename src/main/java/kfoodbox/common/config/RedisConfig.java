6e2372d8790293df7130cd582f9652086eec435d
김주원 <damiano102777@naver.com>
2024-04-06 22:22:23 +0900

    feat: Redis 설정 클래스 정의

---
+++ src/main/java/kfoodbox/common/config/RedisConfig.java
@@ -0,0 +1,37 @@
+package kfoodbox.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Value("${redis.host}")
+    private String host;
+    @Value("${redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+        /*
+            serializer를 명시하지 않으면 RedisTemplate은 key, value 모두 기본적으로 JdkSerializationRedisSerializer를 사용한다.
+            JdkSerializationRedisSerializer는 key를 생성할때 앞에 특정한 unicode가 붙는다.
+            따라서 StringRedisSerializer로 설정을 해주어야 한다.
+         */
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+}
+
