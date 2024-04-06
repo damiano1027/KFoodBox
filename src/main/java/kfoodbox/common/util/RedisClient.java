a53d7d688bdaa1a82532d1693a75a3efaf99b590
김주원 <damiano102777@naver.com>
2024-04-06 23:23:00 +0900

    feat: redis 인증번호 조회 메소드

--- src/main/java/kfoodbox/common/util/RedisClient.java
+++ src/main/java/kfoodbox/common/util/RedisClient.java
@@ -1,10 +1,12 @@
 package kfoodbox.common.util;
 
+import com.amazonaws.services.apigateway.model.Op;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
+import java.util.Optional;
 
 @Component
 public class RedisClient {
@@ -20,5 +22,10 @@ public class RedisClient {
     public void putSignupAuthenticationNumber(String email, String authenticationNumber) {
         String key = String.format("%s/%s", SIGNUP_AUTHENTICATION_NUMBER_PREFIX, email);
         valueOperations.set(key, authenticationNumber, Duration.ofMinutes(SIGNUP_AUTHENTICATION_NUMBER_TIMEOUT_MINUTE));
+    }
+
+    public Optional<String> getSignupAuthenticationNumberAndDelete(String email) {
+        String key = String.format("%s/%s", SIGNUP_AUTHENTICATION_NUMBER_PREFIX, email);
+        return Optional.ofNullable(valueOperations.getAndDelete(key));
     }
 }
