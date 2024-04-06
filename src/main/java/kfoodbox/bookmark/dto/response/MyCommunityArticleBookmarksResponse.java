8ce251a276ab4ca3b83ed959387fdd18286c27e9
김주원 <damiano102777@naver.com>
2024-04-06 16:14:50 +0900

    docs: 북마크 조회 API에서 응답 객체 Swagger 설명 추가

--- src/main/java/kfoodbox/bookmark/dto/response/MyCommunityArticleBookmarksResponse.java
+++ src/main/java/kfoodbox/bookmark/dto/response/MyCommunityArticleBookmarksResponse.java
@@ -1,5 +1,6 @@
 package kfoodbox.bookmark.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,11 +9,14 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 public class MyCommunityArticleBookmarksResponse {
+    @Schema(description = "북마크 리스트")
     private List<Bookmark> bookmarks;
 
     @Getter
     public static class Bookmark {
+        @Schema(description = "게시물 id")
         private Long id;
+        @Schema(description = "제목")
         private String title;
     }
 }
