c1ec598d5fc270b5f5fb4894556501a60d94e608
김주원 <damiano102777@naver.com>
2024-04-10 20:50:09 +0900

    feat: 자유게시판 댓글 수정에 사용될 CommunityComment클래스의 메소드 정의

--- src/main/java/kfoodbox/article/entity/CommunityComment.java
+++ src/main/java/kfoodbox/article/entity/CommunityComment.java
@@ -1,6 +1,7 @@
 package kfoodbox.article.entity;
 
 import kfoodbox.article.dto.request.CommunityCommentCreateRequest;
+import kfoodbox.article.dto.request.CommunityCommentUpdateRequest;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -27,5 +28,17 @@ public class CommunityComment {
 
     public void changeUserId(long userId) {
         this.userId = userId;
+    }
+
+    public boolean hasSameUserId(long userId) {
+        return this.userId.equals(userId);
+    }
+
+    public boolean isUpdateRequired(CommunityCommentUpdateRequest request) {
+        return !content.equals(request.getContent());
+    }
+
+    public void update(CommunityCommentUpdateRequest request) {
+        content = request.getContent();
     }
 }
