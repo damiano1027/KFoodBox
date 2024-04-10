aefd3fee902e4df20f65689f3903a89d31977d67
김주원 <damiano102777@naver.com>
2024-04-10 20:19:35 +0900

    feat: 자유게시판 댓글 생성 SQL

--- src/main/java/kfoodbox/article/repository/CommunityArticleRepository.java
+++ src/main/java/kfoodbox/article/repository/CommunityArticleRepository.java
@@ -3,6 +3,7 @@ package kfoodbox.article.repository;
 import kfoodbox.article.dto.response.CommunityArticleResponse;
 import kfoodbox.article.entity.CommunityArticle;
 import kfoodbox.article.entity.CommunityArticleImage;
+import kfoodbox.article.entity.CommunityComment;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -16,4 +17,5 @@ public interface CommunityArticleRepository {
     void updateCommunityArticle(@Param("communityArticle") CommunityArticle communityArticle);
     void deleteCommunityArticleImages(@Param("communityArticleId") Long communityArticleId, @Param("urls") List<String> urls);
     void deleteCommunityArticleById(@Param("id") Long id);
+    void saveCommunityComment(@Param("communityComment") CommunityComment communityComment);
 }
