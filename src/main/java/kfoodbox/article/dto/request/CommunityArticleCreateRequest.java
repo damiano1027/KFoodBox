759f7ccba0e5b4265a65e7a33c5f4e3bf19f2405
김주원 <damiano102777@naver.com>
2024-04-11 16:02:07 +0900

    fix: 자유게시판 게시물 생성/수정 요청 dto에 잘못 표기된 프로퍼티 의미 수정

--- src/main/java/kfoodbox/article/dto/request/CommunityArticleCreateRequest.java
+++ src/main/java/kfoodbox/article/dto/request/CommunityArticleCreateRequest.java
@@ -18,7 +18,7 @@ public class CommunityArticleCreateRequest {
     private String title;
 
     @NotBlank @Size(min = 1, max = 16777215)
-    @Schema(description = "제목\n" +
+    @Schema(description = "내용\n" +
                           "- Not null\n" +
                           "- 길이가 1 이상 16777215 이하\n" +
                           "- 공백문자만 있으면 안됨\n")
