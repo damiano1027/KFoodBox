da8980035d22fc8db39a8c2d31eba24884a51450
김주원 <damiano102777@naver.com>
2024-04-29 13:45:25 +0900

    feat: 레시피게시판 생성 관련 SQL

--- src/main/java/kfoodbox/article/repository/CustomRecipeArticleRepository.java
+++ src/main/java/kfoodbox/article/repository/CustomRecipeArticleRepository.java
@@ -1,8 +1,17 @@
 package kfoodbox.article.repository;
 
 import kfoodbox.article.entity.CustomRecipeArticle;
+import kfoodbox.article.entity.CustomRecipeArticleImage;
+import kfoodbox.article.entity.CustomRecipeIngredient;
+import kfoodbox.article.entity.CustomRecipeSequence;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 public interface CustomRecipeArticleRepository {
+    void saveCustomRecipeArticle(@Param("customRecipeArticle") CustomRecipeArticle customRecipeArticle);
+    void saveCustomRecipeSequences(@Param("customRecipeSequences") List<CustomRecipeSequence> customRecipeSequences);
+    void saveCustomRecipeIngredients(@Param("customRecipeIngredients") List<CustomRecipeIngredient> customRecipeIngredients);
+    void saveCustomRecipeArticleImages(@Param("customRecipeArticleImages") List<CustomRecipeArticleImage> customRecipeArticleImages);
     CustomRecipeArticle findCustomRecipeArticleById(@Param("id") Long id);
 }
