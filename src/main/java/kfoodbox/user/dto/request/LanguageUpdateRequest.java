b5bae0a0f642a7930f12836a9e243a2ada50247d
김주원 <damiano102777@naver.com>
2024-05-04 12:49:26 +0900

    feat: 요청 dto 제약조건 추가

--- src/main/java/kfoodbox/user/dto/request/LanguageUpdateRequest.java
+++ src/main/java/kfoodbox/user/dto/request/LanguageUpdateRequest.java
@@ -1,8 +1,13 @@
 package kfoodbox.user.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class LanguageUpdateRequest {
+    @NotNull
+    @Schema(description = "언어 id\n" +
+                          "- Not null")
     private Long languageId;
 }
