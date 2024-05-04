9ad9a8b272f67599e4844bb34bf6193bcfd1ca1d
김주원 <damiano102777@naver.com>
2024-05-04 19:39:39 +0900

    feat: query parameter dto 클래스 정의

---
+++ src/main/java/kfoodbox/article/dto/request/CustomRecipeArticlesCondition.java
@@ -0,0 +1,43 @@
+package kfoodbox.article.dto.request;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class CustomRecipeArticlesCondition {
+    @NotNull @Positive
+    private Long page;
+
+    @NotNull @Min(1) @Max(50)
+    private Long limit;
+
+    @NotNull
+    private Type type;
+
+    @NotNull
+    private Sort sort;
+
+    private String query;
+
+    private Long foodId;
+
+    private enum Type {
+        ALL
+    }
+
+    private enum Sort {
+        LATEST, OLDEST, LIKES, COMMENTS
+    }
+
+    public Long calculateTotalPage(Long totalCount) {
+        return totalCount.equals(0L) ? 1L : (long) Math.ceil((double) totalCount / this.limit);
+    }
+
+    public Long calculateCursor() {
+        return (page - 1) * limit;
+    }
+}
